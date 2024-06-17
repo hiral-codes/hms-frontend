81fb1ea494d4810599b4c65dd12c439ab09a7bbf
Hiral Patel <hiralpatel160302@gmail.com>
2024-06-17 16:58:51 +0530

    improved ui

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ import { BsDiscord } from "react-icons/bs";
 function About() {
   return (
     <section className="bg-black">
-      <div className="py-0 px-4 mx-auto max-w-screen-xl lg:py-2 lg:px-6 ">
+      <div className="py-2 px-4 mx-auto max-w-screen-xl lg:py-2 lg:px-6 ">
         <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
             Meet Our Team
